908723bf0acf4da7f936ebb8c83ca0392da5ce66
Gabriel Nützi <gabriel.nuetzi@sdsc.ethz.ch>
2025-02-13 16:50:04 +0100

    init

---
+++ tools/nix/shells/toolchain-python.nix
@@ -0,0 +1,52 @@
+# This function defines attrsets with packages
+# to be used in the different development shells
+# in this folder.
+
+# Search for package at:
+# https://search.nixos.org/packages
+{
+  lib,
+  pkgs,
+  namespace,
+  ...
+}:
+{
+  packages = [
+    pkgs.${namespace}.bootstrap
+    pkgs.${namespace}.treefmt
+
+    # Language Server.
+    pkgs.pyright
+
+    # Formatter and linter.
+    pkgs.ruff
+
+    pkgs.stdenv.cc.cc.lib # fix: libstdc++ required by jupyter.
+    pkgs.libz # fix: for numpy/pandas import
+  ];
+
+  # We use `devenv` language support since, its
+  # pretty involved to setup a python environment.
+  languages.python = {
+    directory = lib.${namespace}.fs.root-dir;
+    enable = true;
+    venv.enable = true;
+    uv = {
+      enable = true;
+      package = pkgs.uv;
+      sync = {
+        enable = true;
+        allExtras = true;
+      };
+    };
+  };
+
+  enterShell = ''
+    just setup
+  '';
+
+  env.LD_LIBRARY_PATH = "${lib.makeLibraryPath [
+    pkgs.stdenv.cc.cc.lib
+    pkgs.libz
+  ]}";
+}
